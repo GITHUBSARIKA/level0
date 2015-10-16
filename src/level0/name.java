fb2a02aa74d0fbf6f493c9864e34b1038ec6bc01
GITHUBSARIKA <Guest@wts8.wts.local>
2015-10-15 18:25:47 -0700

    spudcounter

--- src/level0/name.java
+++ src/level0/name.java
@@ -4,9 +4,9 @@ public class name {
 public static void main(String[] args) {
 	for (int i = 0; i < 9; i++) {
 	System.out.println("sarika");
-	if (name.) {
+	//if (name.) {
 		
 	}
 	}
 }
-}
+
---
+++ src/level0/spudcounter.java
@@ -0,0 +1,18 @@
+package level0;
+
+public class spudcounter {
+	public static void main(String[] args) {
+
+		for (int i = 1; i < 8; i++) {
+			
+			if (i == 4) {
+				System.out.println(i);
+
+			}
+			else{
+				System.out.println(i + "potato");
+			}
+		}
+		System.out.println("more");
+	}
+}
