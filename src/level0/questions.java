1036c5bcbdfc6029cfa2541893e1756b35d2516d
Mrfuzzy <league@WTS9.attlocal.net>
2015-12-03 18:28:02 -0800

    high low

---
+++ src/level0/questions.java
@@ -0,0 +1,52 @@
+package level0;
+
+import javax.swing.JOptionPane;
+
+public class questions {
+public static void main(String[] args) {
+JOptionPane.showInputDialog("How are you doing");
+JOptionPane.showInputDialog("do you like bannanas");
+JOptionPane.showMessageDialog(null,"too bad because i don't like bannanas");
+JOptionPane.showInputDialog("do you like math");
+JOptionPane.showMessageDialog(null,"null");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+}
