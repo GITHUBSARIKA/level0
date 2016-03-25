a27a00edba7a9c18e22549499280b83391168fc6
Revanty <league@WTS14.attlocal.net>
2016-03-24 18:29:21 -0700

    for loop

---
+++ src/level0/guantletforloop899.java
@@ -0,0 +1,39 @@
+package level0;
+
+public class guantletforloop899 {
+	public static void main(String[] args) {
+		// for (int i = 1; i < 101; i++) {
+		// System.out.println(i);
+
+		// for (int i = 100; i > 0; i--) {
+		// System.out.println(i);
+
+		// for (int i = 2; i < 101; i++) {
+		// System.out.println(i);
+
+		// for (int i = 1; i < 100; i += 2) {
+		// syso
+
+		// for (int i = 0; i < 500; i++) {
+		// System.out.print(i);
+
+		// if (i % 2 == 0) {
+		// System.out.println("even");
+		// }
+
+		// if (i % 2 == 1) {
+
+		// System.out.println("odd");
+		// }
+		// }
+		// for (int i = 7; i < 784; i += 7) {
+		// System.out.println(i);
+		int age = 0;
+		for (int i = 2006; i < 2016; i++) {
+			System.out.println("In " + i + "I was" + age + " years old");
+			age += 1;
+		}
+
+	}
+
+}
